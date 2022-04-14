dd895f570e44d4f9462dcf8c60332fa8130c700a
VOID <kouichi.hirachi@gmail.com>
2022-04-15 01:23:05 +0900

    fix nya

--- app/app.py
+++ app/app.py
@@ -89,7 +89,7 @@ async def on_message(message):
                 text = await replaceUserName(text)
             if url.match(text):
                 return
-            jtalk(text + 'にゃ〜')
+            jtalk(text + 'にゃー')
             audio_source = discord.FFmpegPCMAudio('open_jtalk.wav')
             voice.play(audio_source)
 
