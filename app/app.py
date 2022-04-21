a3817da60fc011ea97cd1bac62a5887e565b39b7
VOID <kouichi.hirachi@gmail.com>
2022-04-21 14:28:20 +0900

    add: 色々

--- app/app.py
+++ app/app.py
@@ -5,15 +5,17 @@ import re
 import subprocess
 from pydub import AudioSegment
 import time
-
+from threading import Timer
+from pymongo import MongoClient
+from datetime import datetime
 from collections import defaultdict, deque
 
 queue_dict = defaultdict(deque)
 
 
-def enqueue(voice_client, guild, source):
+def enqueue(voice_client, guild, source, filename):
     queue = queue_dict[guild.id]
-    queue.append(source)
+    queue.append([source, filename])
     if not voice_client.is_playing():
         play(voice_client, queue)
 
@@ -22,11 +24,17 @@ def play(voice_client, queue):
     if not queue or voice_client.is_playing():
         return
     source = queue.popleft()
-    voice_client.play(source, after=lambda e: play(voice_client, queue))
+    # os.remove(source[1])
+    voice_client.play(source[0], after=lambda e: play(voice_client, queue))
 
 
 def current_milli_time():
     return round(time.time() * 1000)
+
+
+async def addDict(arg1, arg2):
+    with open('dict.txt', mode='a') as f:
+        f.write(arg1 + ',' + arg2+'\n')
 
 
 class CommonModule:
@@ -34,6 +42,40 @@ class CommonModule:
         with open(file, 'r', encoding='utf-8') as f:
             json_data = json.load(f)
         return json_data
+
+
+def replaceDict(text):
+    f = open('dict.txt', 'r')
+    lines = f.readlines()
+    print(lines)
+
+    for line in lines:
+        pattern = line.strip().split(',')
+        if pattern[0] in text and len(pattern) >= 2:
+            text = text.replace(pattern[0], pattern[1])
+    f.close()
+    return text
+
+
+def showDict():
+    f = open('dict.txt', 'r')
+    lines = f.readlines()
+    output = "現在登録されている辞書一覧\n"
+    for index, line in enumerate(lines):
+        pattern = line.strip().split(',')
+        output += "{0}: {1} -> {2}\n".format(index+1, pattern[0], pattern[1])
+    f.close()
+    return output
+
+
+async def removeDict(num):
+    try:
+        cmd = ["sed", "-i.bak", "-e", ("{0}d").format(num), "dict.txt"]
+        subprocess.call(cmd)
+    except Exception as e:
+        print(e)
+        return 0
+    return 1
 
 
 async def jtalk(t):
@@ -57,7 +99,7 @@ voice = None
 volume = None
 
 url = re.compile('^http')
-mention = re.compile('<@![^>]*>*')
+mention = re.compile('<@[^>]*>*')
 
 # GUILD_ID=833346660201398282
 # guild = client.get_guild(GUILD_ID)
@@ -74,7 +116,7 @@ async def replaceUserName(text):
         if not mention.match(word):
             continue
         userId = re.sub('[<@!> ]', '', word)
-        # print(userId)
+        print(userId)
         userName = str(await client.fetch_user(userId))
         # nickName = str(await guild.get_member_named(userName))
         # print(nickName)
@@ -114,15 +156,40 @@ async def on_message(message):
             await message.channel.send('音量を下げました')
         elif text == '!bye':
             await client.close()
-        else:
-            print(text)
-            if mention.search(text):
-                text = await replaceUserName(text)
-            if url.match(text):
+        elif text == '!get':
+            await message.channel.send(showDict())
+        elif re.match('^!add', text):
+            args = message.content.split(" ")
+            if len(args) < 3:
+                await message.channel.send("Usage: !add A B")
                 return
-            filename = await jtalk(text)
-            enqueue(message.guild.voice_client, message.guild,
-                    discord.FFmpegPCMAudio(filename))
-            # os.remove(filename)
+            a = args[1]
+            b = args[2]
+            await addDict(a, b)
+            await message.channel.send(("{0}を{1}と読むように辞書に登録しました！").format(a, b))
+        elif re.match('^!remove', text):
+            args = message.content.split(" ")
+            if len(args) < 2:
+                await message.channel.send("Usage: !remove 3")
+                return
+            num = int(args[1])
+            print(await removeDict(num))
+            if await removeDict(num):
+                await message.channel.send("削除しました")
+            else:
+                await message.channel.send("エラーが発生しました")
+        else:
+            print(message.guild.voice_client is True)
+            if message.guild.voice_client:
+                if mention.search(text):
+                    text = await replaceUserName(text)
+                text = re.sub('http.*', '', text)
+                text = replaceDict(text)
+                filename = await jtalk(text)
+                enqueue(message.guild.voice_client, message.guild,
+                        discord.FFmpegPCMAudio(filename), filename)
+                timer = Timer(3, os.remove, (filename, ))
+                timer.start()
+                # os.remove(filename)
 
 client.run(client_id)
